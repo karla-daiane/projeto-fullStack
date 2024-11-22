cad5054dff5939e7c792be9a34ad420cca8fdb30
Karla Daiane <karladaiane.dev@gmail.com>
2024-11-22 10:53:13 -0300

    att MinRoutes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import MainRoutes from './routes'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename='/projeto-fullStack'>
         <MainRoutes />
     </BrowserRouter>
 )
